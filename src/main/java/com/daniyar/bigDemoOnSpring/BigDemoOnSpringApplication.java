4a01bc6bf9f24ef587669332d7ad6ff67215487a
Daniar <daniyaruteg123@gmail.com>
2024-06-02 15:13:49 +0500

    commit

---
+++ src/main/java/com/daniyar/bigDemoOnSpring/BigDemoOnSpringApplication.java
@@ -0,0 +1,13 @@
+package com.daniyar.bigDemoOnSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BigDemoOnSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BigDemoOnSpringApplication.class, args);
+	}
+
+}
